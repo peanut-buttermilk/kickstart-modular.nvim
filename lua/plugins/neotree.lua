d3aaefd2e4c7340be03b81f6794b9e02374eb7cc
Balaji Cherukuri <balusoft@gmail.com>
2024-03-27 13:45:13 -0700

    adding auto close

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,7 +7,13 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
     vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    require('neo-tree').setup{
+      auto_close = true,
+      update_focused_file = {
+        enable = true,
+      },
+    }
   end,
 }
